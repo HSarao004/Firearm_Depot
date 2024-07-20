e5c96af07df4c081652113507011c4e454734543
Hsarao2 <hsarao2@rrc.ca>
2024-07-19 19:56:17 -0500

    logout done

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-
+  # GET /resource/sign_up
   def new
     super
   end
 
+  # POST /resource
   def create
     build_resource(sign_up_params)
 
@@ -14,31 +14,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
-        respond_to do |format|
-          format.html { redirect_to after_sign_up_path_for(resource) }
-          format.json { render json: resource, status: :created }
-        end
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
-        respond_to do |format|
-          format.html { redirect_to after_inactive_sign_up_path_for(resource) }
-          format.json { render json: resource, status: :created }
-        end
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
       set_minimum_password_length
-      respond_to do |format|
-        format.html { render :new }
-        format.json { render json: resource.errors, status: :unprocessable_entity }
-      end
+      respond_with resource
     end
   end
 
-  private
+  protected
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
   end
 end
